236962045e6c2031a9c5e8c682541410b2c4a4d4
Gamze Yaş | 1B Mobile <148863631+gamzeyas@users.noreply.github.com>
2023-11-22 10:11:57 +0300

    16 kasım workshop

--- lib/pages/expenses_page.dart
+++ lib/pages/expenses_page.dart
@@ -5,38 +5,48 @@ import 'package:expenseapp/widgets/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesPage extends StatefulWidget {
-  const ExpensesPage({Key? key}) : super(key: key);
+  const ExpensesPage(this.expenses, this.onRemove, {Key? key})
+      : super(key: key);
+  final List<Expense> expenses;
+  final void Function(Expense expense) onRemove;
 
   @override
   _ExpensesPageState createState() => _ExpensesPageState();
 }
 
 class _ExpensesPageState extends State<ExpensesPage> {
-  List<Expense> expenses = [
-    Expense(name: "Yemek", price: 500.529, date: DateTime.now()),
-    Expense(name: "Udemy Kursu", price: 200, date: DateTime.now()),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const SizedBox(
+        SizedBox(
           height: 150,
-          child: Text("Grafik Bölümü"),
+          child: Text(
+            "Grafik Bölümü",
+            style: Theme.of(context).textTheme.titleLarge,
+          ), // titleLarge stilini alması
         ),
         Expanded(
           child: ListView.builder(
-              itemCount: expenses.length,
+              itemCount: widget.expenses.length,
               itemBuilder: (context, index) {
-                return ExpenseItem(expenses[index]);
+                return Dismissible(
+                  key: ValueKey(widget.expenses[index]),
+                  child: ExpenseItem(widget.expenses[index]),
+                  onDismissed: (direction) {
+                    // if (direction == DismissDirection.startToEnd) {
+                    //   // eğer soldan sağa kaydırılmışsa..
+                    // }
+                    //print(direction);
+                    widget.onRemove(widget.expenses[index]);
+                  },
+                );
               }),
-        ),
-        const SizedBox(
-          height: 150,
-          child: Text("Burası bottom bar."),
         )
       ]),
     );
   }
 }
+
+// listeden veri silme ve alt başlıkları
+// theming ve alt başlıkları
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,31 +1,49 @@
+import 'package:expenseapp/models/expense.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({Key? key}) : super(key: key);
-
+  const NewExpense(this.onAdd, {Key? key}) : super(key: key);
+  final void Function(Expense expense) onAdd;
   @override
   _NewExpenseState createState() => _NewExpenseState();
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  DateTime? _selectedDate;
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime.now().subtract(Duration(days: 365)),
-      lastDate: DateTime.now(),
-    );
-    if (picked != null && picked != _selectedDate) {
-      setState(() {
-        _selectedDate = picked;
-      });
-    }
-  }
-
   var _expenseNameController = TextEditingController();
   var _expensePriceController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.work;
+
+  void _openDatePicker() async {
+    DateTime today = DateTime.now(); // 16.11.2023
+    // 2022, 11, 16
+    DateTime oneYearAgo = DateTime(today.year - 1, today.month, today.day);
+    // showDatePicker(
+    //         context: context,
+    //         initialDate: today,
+    //         firstDate: oneYearAgo,
+    //         lastDate: today)
+    // .then((value) {
+    //   async işlemden cevap ne zaman gelirse bu bloğu çalıştır..
+    //   print(value);
+    // });
+    // async function => await etmek
+    // nullable
+    // 14:20
+    DateTime? selectedDate = await showDatePicker(
+        context: context,
+        initialDate: _selectedDate == null ? today : _selectedDate!,
+        firstDate: oneYearAgo,
+        lastDate: today);
+    setState(() {
+      _selectedDate = selectedDate;
+    });
+    print("Merhaba");
+    // sync => bir satır çalışmasını bitirmeden alt satıra geçemez.
+    // async => async olan satır sadece tetiklenir kod aşağıya doğru çalışmaya devam eder
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -38,26 +56,75 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: InputDecoration(labelText: "Harcama Adı"),
           ),
-          TextField(
-            controller: _expensePriceController,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(labelText: "Harcama Miktarı"),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _expensePriceController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      labelText: "Harcama Miktarı", prefixText: "₺"),
+                ),
+              ),
+              IconButton(
+                  onPressed: () => _openDatePicker(),
+                  icon: const Icon(Icons.calendar_month)),
+              // ternary operator
+              Text(_selectedDate == null
+                  ? "Tarih Seçiniz"
+                  : DateFormat.yMd().format(_selectedDate!)),
+            ],
+            // String?  a
+            // String => a!
           ),
-          IconButton(
-            onPressed: () => _selectDate(context),
-            icon: Icon(Icons.calendar_month),
+          const SizedBox(
+            height: 30,
           ),
-          Text(
-            _selectedDate == null
-                ? "Tarih Seçiniz"
-                : DateFormat('yyyy-MM-dd').format(_selectedDate!),
+          Row(
+            children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values.map((category) {
+                    return DropdownMenuItem(
+                        value: category, child: Text(category.name));
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      if (value != null) _selectedCategory = value;
+                    });
+                  })
+            ],
           ),
-          ElevatedButton(
-              onPressed: () {
-                print(
-                    "Kaydedilen değer: ${_expenseNameController.text} ${_expensePriceController.text}");
-              },
-              child: Text("Ekle"))
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Kapat")),
+              const SizedBox(
+                width: 12,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    double? price =
+                        double.tryParse(_expensePriceController.text);
+
+                    // validation
+
+                    Expense expense = Expense(
+                        name: _expenseNameController.text,
+                        price: price!,
+                        date: _selectedDate!,
+                        category: _selectedCategory);
+                    widget.onAdd(expense);
+                    Navigator.pop(context);
+                  },
+                  child: Text("Ekle")),
+            ],
+          ),
         ]),
       ),
     );
